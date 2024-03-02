212674936766303c4151a4d949258acb8d383717
Ashish Debnath <ashishume@gmail.com>
2024-03-02 14:45:46 +0530

    feat: mobile responsive

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -3,11 +3,13 @@ import { useNavigate } from "react-router-dom";
 import { INavbar, NavItems } from "../../Shared/contants";
 import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { SVGs } from "../../Shared/icons";
 
 const Navbar = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const [activeItemIndex, setActiveItemIndex] = useState(null as any);
+  const [open, setOpen] = useState(false);
   useEffect(() => {
     const { pathname } = location;
 
@@ -30,25 +32,42 @@ const Navbar = () => {
   const handleNavItem = (route: string) => {
     navigate(route);
   };
+  const toggleNavbar = () => {
+    setOpen(!open);
+  };
 
   return (
     <div className={style["container"]}>
       <ul className={style["nav-items"]}>
-        {NavItems.map((item, index) => {
-          return (
-            <li
-              key={item.label}
-              className={
-                activeItemIndex !== null && index == activeItemIndex
-                  ? style["active"]
-                  : ""
-              }
-              onClick={() => handleNavItem(item?.route)}
-            >
-              {item.label}
-            </li>
-          );
-        })}
+        <div className={style["navbar-resp"]}>
+          {open ? (
+            <div className={style["close-icon"]} onClick={toggleNavbar}>
+              {SVGs().CloseIcon}
+            </div>
+          ):
+            <div className={style["menu-icon"]} onClick={toggleNavbar}>
+              {SVGs().MenuIcon}
+            </div>
+          
+          }
+        </div>
+
+        {open &&
+          NavItems.map((item, index) => {
+            return (
+              <li
+                key={item.label}
+                className={
+                  activeItemIndex !== null && index == activeItemIndex
+                    ? style["active"]
+                    : ""
+                }
+                onClick={() => handleNavItem(item?.route)}
+              >
+                {item.label}
+              </li>
+            );
+          })}
       </ul>
     </div>
   );
--- src/Pages/About/index.tsx
+++ src/Pages/About/index.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import Layout from "../../Layout/layout";
-
+import "./style.scss";
 const About = () => {
   return (
     <Layout>
-      <div>About</div>
+      <div className="about-container">
+        <div className="content">
+        <h2>About Me</h2>
+          Hello! I'm Ashish Debnath, a 26-year-old software engineer
+          specializing in frontend development. With around 5 years of
+          experience in the field, I've honed my skills in React, Angular,
+          JavaScript, Node.js, TypeScript, and various databases. Originally
+          from the vibrant state of West Bengal, India, I bring a blend of
+          cultural diversity and technical expertise to my work. Passionate
+          about creating seamless user experiences and leveraging technology to
+          solve real-world problems, I'm always eager to embark on new
+          challenges and contribute to innovative projects. Let's connect and
+          build something great together!
+        </div>
+      </div>
     </Layout>
   );
 };
